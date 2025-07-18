5025f783a229424f68f849ec006d429e0388e300
tan x dx <andrewhayman19@gmail.com>
2025-07-18 10:07:29 +0100

    Added some type aliases.

---
+++ UFD/TypeAliases.cs
@@ -0,0 +1,3 @@
+﻿global using EisensteinIntegerFactorisation = UFD.FactorisationMethods<UFD.UfdElements.EisensteinIntegers.EisensteinPrimeIdentifier, UFD.UfdElements.EisensteinIntegers.EisensteinInteger>;
+global using GaussianIntegerFactorisation = UFD.FactorisationMethods<UFD.UfdElements.GaussianIntegers.GaussianPrimeIdentifier, UFD.UfdElements.GaussianIntegers.GaussianInteger>;
+global using IntFactorisation = UFD.FactorisationMethods<UFD.UfdElements.IntegerPrimeIdentifier, int>;
