8762dbcafbe005b1e89a3a799a64ef9c3f2ab33a
tan x dx <andrewhayman19@gmail.com>
2025-07-26 16:23:23 +0100

    Added evaluation of a fun function from Discord.

--- UFD/Program.cs
+++ UFD/Program.cs
@@ -1,9 +1,24 @@
-﻿using UFD.UfdElements;
+﻿using System.Numerics;
+using UFD.UfdElements;
 using UFD.UfdElements.GaussianIntegers;
 
 IntegerMethods.Initialise(100_000_000);
 GaussianIntegerMethods.Initialise(10_000);
 
+for (var i = 1; i <= 1000; i++)
+{
+    try
+    {
+        Console.WriteLine($"f({i}) = {F(i)}");
+    }
+    catch
+    {
+    }
+}
+
+;
+
+/*
 var primes = IntegerMethods.GetAllPrimes();
 
 var n = 11638725;
@@ -13,3 +28,26 @@ Console.WriteLine($"{n} = {string.Join('*', primeFactorsOfN)}");
 var g = 13 + 21.i();
 var primeFactorsOfG = GaussianIntegerFactorisation.GetPrimeFactors(g);
 Console.WriteLine($"{g} -> {string.Join(',', primeFactorsOfG)}");
+*/
+
+BigInteger F(BigInteger n)
+{
+    if (n <= BigInteger.One)
+        return BigInteger.One;
+
+    int k = (int)n;
+    var primeFactorsOfN = IntFactorisation.GetPrimeFactors(k);
+
+    BigInteger total = BigInteger.One;
+
+    foreach (var primeFactor in primeFactorsOfN)
+    {
+        BigInteger bigPrimePart = primeFactor - 1;
+
+        var evaluatedPrimePart = F(bigPrimePart);
+
+        total *= (evaluatedPrimePart * F(evaluatedPrimePart)) + 1;
+    }
+
+    return total;
+}
